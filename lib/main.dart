b10681c40356c3ef24e119d465290dc764d96b74
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2022-08-08 23:43:23 +0300

    Lesson 76: Code challenge complete.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'question.dart';
 import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 QuizBrain quizBrain = QuizBrain();
 
@@ -25,7 +26,8 @@ class Quizzler extends StatelessWidget {
 }
 
 class QuizPage extends StatefulWidget {
-  @override
+  //extends is used to inherit properties of the parent
+  @override //overrides is used to customize some of the inherited properties
   _QuizPageState createState() => _QuizPageState();
 }
 
@@ -44,6 +46,62 @@ class _QuizPageState extends State<QuizPage> {
   //   true
   // ];
 
+  //we use void below because this is a method inside a class
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain
+        .getCorrectAnswer(); //new way to access the answers from question.dart
+
+    setState(() {
+      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If so,
+      //On the next line, you can also use if (quizBrain.isFinished()) {}, it does the same thing.
+      if (quizBrain.isFinished() == true) {
+        //TODO Step 4 Part A - show an alert using rFlutter_alert,
+
+        //This is the code for the basic alert from the docs for rFlutter Alert:
+        //Alert(context: context, title: "RFLUTTER", desc: "Flutter is awesome.").show();
+
+        //Modified for our purposes:
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+        ).show();
+
+        //TODO Step 4 Part C - reset the questionNumber,
+        quizBrain.reset();
+
+        //TODO Step 4 Part D - empty out the scoreKeeper.
+        scoreKeeper = [];
+      }
+
+      //TODO: Step 6 - If we've not reached the end, ELSE do the answer checking steps below 👇
+      else {
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
+  //   if(userPickedAnswer == correctAnswer){
+  //     scoreKeeper.add(Icon(Icons.check, color: Colors.green,));
+  //   } else {
+  //     scoreKeeper.add(Icon(Icons.close, color: Colors.red,));
+  //   }
+  //
+  //     quizBrain.nextQuestion();
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -57,7 +115,8 @@ class _QuizPageState extends State<QuizPage> {
             child: Center(
               child: Text(
                 // questions[questionNumber],
-                quizBrain.getQuestionText(), //new way to access questions and the answers after putting them in an array
+                quizBrain
+                    .getQuestionText(), //new way to access questions and the answers after putting them in an array
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -84,16 +143,7 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked true.
                 // quizBrain.questionBank[questionNumber].questionAnswer;
                 // bool correctAnswer = answers[questionNumber];
-                bool correctAnswer = quizBrain.getCorrectAnswer();//new way to access the answers from question.dart
-                if(correctAnswer==true){
-                  print("User got it right!");
-                } else {
-                  print("User got it wrong!");
-                }
-
-                setState(() {
-                  quizBrain.nextQuestion();
-                });
+                checkAnswer(true);
               },
             ),
           ),
@@ -115,16 +165,7 @@ class _QuizPageState extends State<QuizPage> {
 
                 // bool correctAnswer = answers[questionNumber];
                 // bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer; //refactored
-                bool correctAnswer = quizBrain.getCorrectAnswer();
-                if(correctAnswer==false){
-                  print("User got it right!");
-                } else {
-                  print("User got it wrong!");
-                }
-
-                setState(() {
-                  quizBrain.nextQuestion();
-                });
+                checkAnswer(false);
               },
             ),
           ),
